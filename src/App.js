a514ac358e0ade3f3059ccc0c815881badbd860c
Chester Lam <chesterlam620@gmail.com>
2017-06-04 14:40:40 +1000

    Add authetication layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello world
+      <div className="container-center">
+        <div className="container-center panel panel-default">
+            <Button className= "" bsStyle="primary" bsSize="large">Register</Button>
+            <Button bsStyle="success v-space" bsSize="large">Login</Button>
+        </div>
       </div>
     );
   }
